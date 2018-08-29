d2ac41657efddd646d514442a5885ec79fd19332
Tsai Chung-Ting <ting@nlplab.cc>
2018-08-29 23:41:50 +0800

    123

--- .ipynb_checkpoints/Make_Sentence_with_NER-checkpoint.py
+++ .ipynb_checkpoints/Make_Sentence_with_NER-checkpoint.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
 import glob
+from bs4 import BeautifulSoup
 import re
 import csv
 import spacy
@@ -10,8 +10,26 @@ from flashtext import KeywordProcessor
 nlp = spacy.load('en_core_web_sm')
 keyword_processor = KeywordProcessor(case_sensitive=True)
 
-    
-    
+## 讀取黑白名單
+def Load_WhilteBlack(path):
+    print('Loading.......white&Black list...............')
+    df_e = pd.read_csv(path)
+    Ent = []
+    for ent , label in zip(df_e['Entity'],df_e['islabel']):
+        if label == 1:
+            Ent.append(ent)
+    Ent =[i.replace('LLC','').replace('Ltd','').replace('Inc','').replace(',','').replace('N.V','').replace('Co.','').replace('.','').strip() for i in Ent]
+    for i in Ent:keyword_processor.add_keyword(i)
+    return
+
+## 找出含有ORG的句子(使用NER+白名單)
+def Have_NER_sentence(Total_sen ,Main_Company , Year):
+    Tmp = []
+    for s in Total_sen:
+        Ent = keyword_processor.extract_keywords(s)
+        for e in Ent:
+            Tmp.append((s , Main_Company,e , Year))
+    return Tmp
 ## 處理特殊狀況的句子
 def Filter_Title(sen):
     flag = False
@@ -50,56 +68,32 @@ def Deal_html_return_sen(html_list):
         Sen+=nltk_sentence_token(Content)
     return Sen
 
-## 讀取黑白名單
-def Load_WhilteBlack(path):
-    print('Loading.......white&Black list...............')
-    df_e = pd.read_csv(white_black)
-    Ent = []
-    for ent , label in zip(df_e['Entity'],df_e['islabel']):
-        if label == 1:
-            Ent.append(ent)
-    Ent =[i.replace('LLC','').replace('Ltd','').replace('Inc','').replace(',','').replace('N.V','').replace('Co.','').replace('.','').strip() for i in Ent]
-    for i in Ent:keyword_processor.add_keyword(i)
-    return
 
-## 找出含有ORG的句子(使用NER+白名單)
-def Have_NER_sentence(Total_sen):
-    Tmp = []
-    for s in Total_sen:
-        Ent = keyword_processor.extract_keywords(s)
-        for e in Ent:
-            Tmp.append((s , Main_Company,e , Year))
-    return Tmp
-
-## 吃cmd line 參數 (公司、年份、白名單)
-html_fold = sys.argv[1]
-year = sys.argv[2]
-white_black = sys.argv[3]
-html_list = glob.glob(html_fold+year+"*.html")
-
-## 處理後共有幾個句子(尚未用NER)
-Total_sen = Deal_html_return_sen(html_list)
-print('Have : ',len(Total_sen))
-
-## 友善提醒有沒有抓對正確的年份跟公司
-Main_Company = html_list[0].split('/')[1]
-Year = html_list[0].split('/')[-1].split('-')[0].replace('.html','')
-print('Company : ' , Main_Company)
-print('Year : ' , Year)
-
-#導入白名單
-Load_WhilteBlack(white_black )
-#使句子含有ORG
-Result = Have_NER_sentence(Total_sen)
-
-print('Have : ',len(Result))
-
-#將含有ORG的句子、公司、公司對應的公司(NER)、年份及關係存下來       
-import csv
-with open('test.sentence.csv','w') as f:
-    writer = csv.writer(f)
-    writer.writerows([('Sentence','Main','Secondary','Time','Label')])
-    writer.writerows(Result)
-    
-    
-print('Sucess!!')
+def choose_n_company(i):
+    years = ['2012','2013','2014','2015','2016','2017']
+    company_list = glob.glob('xxxx/*/')[:i]
+    #print(company_list)
+    Company_sen = []
+    Company_ent = []
+    with open('test.sentence.csv','a') as f:
+        writer = csv.writer(f)
+        writer.writerows([('Sentence','Main','Secondary','Time','Label')])
+    for i in company_list:
+        print(i)
+        for year in years:
+            html_list = glob.glob(i+'html/'+year+'*.html')
+            Main_Company = html_list[0].split('/')[1]
+            Year = html_list[0].split('/')[-1].split('-')[0].replace('.html','')
+            #print(Main_Company)
+            #print(Year)
+            #print(html_list)
+            Company_sen = Deal_html_return_sen(html_list)
+            Result = Have_NER_sentence(Company_sen,Main_Company,Year)
+            with open('test.sentence.csv','a') as f:
+                writer = csv.writer(f)
+                writer.writerows(Result)
+                
+                
+Load_WhilteBlack('flair.csv')
+choose_n_company(5)
+print("Finish........")
